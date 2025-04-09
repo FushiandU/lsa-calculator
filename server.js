77216eaad63933089f2d8cfd797eb893f65ff332
Fushiandu <fushiandu@gmail.com>
2025-04-10 02:59:49 +0500

    Fix Vercel build process and Playwright installation

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { chromium } = require('playwright-core');
+const { chromium } = require('playwright');
 const cors = require('cors');
 const morgan = require('morgan');
 const logger = require('./logger');
